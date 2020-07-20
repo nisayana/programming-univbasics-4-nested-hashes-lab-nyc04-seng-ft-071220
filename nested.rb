ef22cd328e1631f7dc9553c03ed334f2be8e8b6d
Sayana <nisayana23@yandex.ru>
2020-07-20 19:45:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[0] = {:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]}
+	}
 end
 
 def changing_alan
